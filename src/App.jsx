2ae9250bc4b7d4e74a7f408c64a46ad62b8644ef
Jophila <120914143+ValoriaStudios@users.noreply.github.com>
2023-01-28 16:49:06 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import './App.css';
 import Level01 from "./pages/Level01"
 
 export default function App() {
   return (
-    <main>
+    <div>
+      Hallo
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Level01 />}>
-                  </Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Level01 />}>
+                    </Route>
+        </Routes>
     </BrowserRouter>
       
-    </main>
+    </div>
   )
 }
