f9ee9044f0746d043f156a002e3f4603e129e5b6
ValoriaStudios <heyjohn655@gmail.com>
2023-01-28 18:50:11 +0100

    text

---
+++ src/Pages/Level01.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Level01 = () => {
+  return (
+    <div>Level01</div>
+  )
+}
+
+export default Level01
---
+++ src/Pages/Level02.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Level02 = () => {
+  return (
+    <div>Level02</div>
+  )
+}
+
+export default Level02
