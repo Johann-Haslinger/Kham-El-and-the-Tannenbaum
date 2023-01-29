e4f36ce62084886d031e00ec599e69405c3364ea
ValoriaStudios <heyjohn655@gmail.com>
2023-01-29 08:28:40 +0100

    Project Setup commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
-import { Provider } from "react-redux";
-import {createStore, applyMiddleware, compose } from "redux";
 
 import './index.css';
 import App from './App';
 import { ContextProvider } from "./contexts/ContextProvider";
 
 
+
 ReactDOM.render(
-    <Provider >
-        <ContextProvider>
-            <App />
-        </ContextProvider>  
-    </Provider>,
- 
+    <ContextProvider>
+        <App />
+    </ContextProvider>,
     document.getElementById('root')
 );
-
