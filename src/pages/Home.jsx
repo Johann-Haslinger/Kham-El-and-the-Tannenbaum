250ab646923732f1e01e3a97421829ea1f29149a
ValoriaStudios <heyjohn655@gmail.com>
2023-02-17 08:01:09 +0100

    ImagesName

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,12 @@ import KamEL from "../Images/KamEl.jpeg"
 
 const Home = () => {
   return (
-    <div className="text-3xl font-bold ">Wir sind Bosses 123</div>
+    <div>
+      <div className='w-full flex justify-center mt-7'>
+        <div className="text-3xl font-bold ">Wir sind Bosses </div>
+      </div>
+      <img src={KamEL} />
+    </div>
   )
 }
 
